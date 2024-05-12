566fdceb58a6bbc0ff9c7d49bafa59bb34c14001
Davi1ss <143420728+Davi1ss@users.noreply.github.com>
2024-05-11 17:53:10 -0700

    Update Invaders.java
    changes to "Controlando el caos".

--- SpaceInvaders/Invaders.java
+++ SpaceInvaders/Invaders.java
@@ -20,10 +20,13 @@ public class Invaders extends Canvas {
     // Declarar constantes para el tamaño de la ventana
     public static final int WIDTH = 640; /*Anchura*/
     public static final int HEIGHT = 480; /*Altura*/
+    public static final int SPEED = 10; /*Velocidad */
 
     
     public HashMap<String, BufferedImage> sprites = new HashMap<>();// Crear un HashMap parametrizado para almacenar imágenes con nombres
-    public int posX, posY;// Declarar variables para la posición del objeto en la ventana
+    public int posX, posY, vX;// Declarar variables para la posición del objeto en la ventana
+
+    public BufferedImage buffer;//Declara la variable buffer de la imagen, importando la clase BufferedImage
 
     // Constructor de la clase Invaders
     public Invaders() {
@@ -31,7 +34,11 @@ public class Invaders extends Canvas {
         // Calcular la posición inicial del objeto en la ventana
         posX = WIDTH / 2;
         posY = HEIGHT / 2;
-
+        
+        vX = 2; //se establece la velocidad inicial
+        
+        //se crea un nuevo objeto para el buffer con el mismo ancho y alto que la ventana del juego.
+        buffer = new BufferedImage(WIDTH,HEIGHT, BufferedImage.TYPE_INT_RGB); 
         
         JFrame ventana = new JFrame("Invaders");// Crear una nueva ventana JFrame llamada "Invaders"
         JPanel panel = (JPanel) ventana.getContentPane();// Obtener el panel de contenido de la ventana
@@ -45,7 +52,9 @@ public class Invaders extends Canvas {
         
         ventana.setBounds(0, 0, WIDTH, HEIGHT);// Configurar el tamaño y la posición de la ventana (la posicion x, posicion y, Anchura, Altura)
         ventana.setVisible(true);// Hacer visible la ventana
-        ventana.addWindowListener(new WindowAdapter() {// Agregar un escuchador de eventos para cerrar la ventana correctamente
+
+        // Agregar un escuchador de eventos para cerrar la ventana correctamente
+        ventana.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
 
                 System.exit(0);//Sirve para cuando se presione la "X" de la ventana, se cierre y deje de ejecutar el programa
@@ -85,26 +94,50 @@ public class Invaders extends Canvas {
     }
 
     // Método para dibujar en la ventana
-    public void paint(Graphics g) {
-        
-        g.drawImage(getSprite("bicho.gif"), posX, posY, this);// Dibujar la imagen del "bicho" en la posición especificada
+    public void paintWorld(){
+
+        Graphics g = buffer.getGraphics();
+        g.setColor(getBackground()); //se le asigna le color al grafico y devuelve el color de fondo del objeto canvas
+        g.fillRect(0,0,getWidth(),getHeight()); // dibuja un cuadrado del tamaño de la ventana y lo rellena con el color de fondo
+
+        g.drawImage(getSprite("bicho.gif"), posX, posY,this);// dibuja en el buffer la imagen bicho y lo ordena en las cordenadas
+        getGraphics().drawImage(buffer,0,0,this);//finalmente se dibuja el contenido del buffer en la ventana, todo esto en la coordenadas
     }
+
+    public void paint(Graphics g) {}
+    public void update(Graphics g) {}
 
     // Método para actualizar la posición del objeto en la ventana
     public void updateWorld() {
 
-        // Generar nuevas coordenadas aleatorias para el objeto
-        posX = (int) (Math.random() * WIDTH);
-        posY = (int) (Math.random() * HEIGHT);
+        posX += vX;//aqui se incrementa la posicion horizontal por la cantidad de la velocidad
+
+        /*Esto significa que el objeto se moverá hacia la derecha si vX es positivo y 
+        hacia la izquierda si vX es negativo. */
+
+        if (posX < 0 || posX > WIDTH) vX = -vX;
+        /* Esta es una condición que verifica
+        Si el objeto ha llegado al borde izquierdo (posX < 0) o al borde derecho (posX > WIDTH) de la ventana del juego. 
+        Si el objeto ha llegado a uno de estos bordes, cambia la dirección del movimiento multiplicando vX por -1. 
+        Esto invierte la dirección del movimiento, haciendo que el objeto se mueva en la dirección opuesta. */
+
     }
 
     // Método para ejecutar el juego
     public void game() {
-        // Bucle principal del juego
+
+        //Esto establece un bucle que se ejecutará mientras la ventana del juego sea visible
         while (isVisible()) {
             
-            updateWorld();// Actualizar la posición del objeto
-            paint(getGraphics());// Volver a dibujar la ventana
+            updateWorld();// para actualizar la posición del objeto en la ventana.
+            paintWorld();// para dibujar el contenido de la ventana del juego.
+            paint(getGraphics());// Llama al método paint() con el contexto gráfico de la ventana del juego para volver a dibujar la ventana.
+
+            try {
+                Thread.sleep(SPEED);
+            } catch (InterruptedException e) {}
+            /*Esto pausa la ejecución del hilo actual durante un tiempo determinado. En este caso,
+            el tiempo de pausa está determinado por la constante SPEED, que representa la velocidad del juego.*/
         }
     }
 
